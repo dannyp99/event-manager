333299a2681bee1060168db4c59fb73f3e2f434f
dannyp99 <dannypires99@gmail.com>
2020-05-07 16:25:28 -0400

    server fully calls DB

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const Sugar = require('sugar')
 const sql = require('./database.js')
 Sugar.extend()
 
-let file_server = KoaStatic("../web")
+let file_server = KoaStatic(`${__dirname}/../web`)
 console.log(sql)
 
 router.get('/events', async (context) => {
@@ -18,7 +18,6 @@ router.get('/events', async (context) => {
     const query = 'SELECT * FROM event_manager.event_table'
     try {
 		const data = await sql.con.query(query);
-        //console.log(data[0].event_id);
         data.forEach(item => {
             eventList.push({id: item.event_id, name: item.event_name, date: item.event_date_str})
         })
@@ -29,21 +28,11 @@ router.get('/events', async (context) => {
 
 router.post("/events", async (context) => {
     let newEvent = context.request.body
-    newEvent.id = eventList.length + 1
-    let query = `SELECT * FROM event_manager.event_table WHERE event_id = ${sql.con.escape(newEvent.id)}`
+    if(!(newEvent.name && newEvent.date)){return}
+    const query = `INSERT INTO event_manager.event_table (event_name, event_date, event_date_str) 
+        VALUES (${sql.con.escape(newEvent.name)}, NOW(), ${sql.con.escape(newEvent.name)})`
     try{
         let data = await sql.con.query(query)
-        if(data.length === 1){
-            query = `DELETE FROM event_manager.event_table WHERE event_id = ${sql.con.escape(newEvent.id)}`
-            try {
-                await sql.con.query(query)
-            }catch(err){
-                console.log(err)
-            }
-        }else{
-            if(data.length === 0){ console.log("No results returned") }
-            else{ console.log("Several items returned for the same ID ERROR") }
-        }
     }catch(err){
         console.log(err)
     }
@@ -65,6 +54,7 @@ router.post("/events", async (context) => {
 
 router.delete("/events/:id", async (context) => {
     let eventID = Number(context.params.id)
+    if(eventID < 0){return}
     let query = `SELECT * FROM event_manager.event_table WHERE event_id = ${sql.con.escape(eventID)}`
     try{
         let data = await sql.con.query(query)
