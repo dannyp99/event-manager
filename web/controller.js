9a10f35196f3a7d88febefce323110842aa00422
dannyp99 <dannypires99@gmail.com>
2020-05-14 18:11:44 -0400

    Abstracted View Partly

--- web/controller.js
+++ web/controller.js
@@ -1,6 +1,7 @@
 import Event from "./event.js"
 import Sync from "./sync.js"
 import Helper from "./helper.js"
+import View from "./eventView.js"
 
 class EventController {
 
@@ -9,10 +10,9 @@ class EventController {
         this.helper = new Helper()
         this.events = []
         this.getEvents()
-        var addButton = document.getElementById('addEvent')
-        addButton.addEventListener('click', (event) => this.addEventButton())
-        var deleteButton = document.getElementById('removeEvent')
-        deleteButton.addEventListener('click', (event)=> this.deleteEventButton())
+        this.view = new View(document.querySelector('calendar-view'))
+        this.view.addEventListener('add_event', event => this.addEventButton(event.target.event.name.value, event.target.event.date.value))
+        this.view.addEventListener('del_event', event => this.delEventButton(event.target.event.name.value, event.target.event.date.value))
     }
 
     async getEvents(){
@@ -28,7 +28,7 @@ class EventController {
         eventContent.id = event.id
         eventContent.innerText = `${event.name}: ${event.date}`
         eventContent.addEventListener('click', (event)=> {
-            this.updateEventModifier(event.target)
+            this.view.updateEventModifier(event.target)
         })
         return eventContent
     }
@@ -107,39 +107,34 @@ class EventController {
         }
     }
 
-    async addEventButton(){
-        const eventName = document.getElementById('eventName').value
-        const eventDate = document.getElementById('eventDate').value
+    async addEventButton(eventName, eventDate){
+        console.log(eventName)
+        console.log(eventDate)
         if(eventName === '' || eventDate === ''){return}
-        const eventDateAsDate = new Date(eventDate)
-        eventDateAsDate.setDate(eventDateAsDate.getDate() + 1)
-        if(this.helper.isDuplicateEvent(eventName, eventDateAsDate, this.events)){return}
-        const newEvent = {id: 0, name: eventName, date: eventDateAsDate.toLocaleDateString()}
-        let resp = await this.sync.eventListPost(newEvent)
-        if(resp){
-            console.log(resp)
-            const addedEvent = resp.map(event => new Event(event))
-            this.events = this.events.concat(addedEvent)
-            const content = addedEvent[0]
-            this.updateEventList(content,false)
-            if(this.helper.isThisWeek(eventDateAsDate)){
-                this.updateCalendar(content,false)
+        if(eventName && eventDate) {
+            const eventDateAsDate = new Date(eventDate)
+            eventDateAsDate.setDate(eventDateAsDate.getDate() + 1)
+            if(this.helper.isDuplicateEvent(eventName, eventDateAsDate, this.events)){return}
+            const newEvent = {id: 0, name: eventName, date: eventDateAsDate.toLocaleDateString()}
+            let resp = await this.sync.eventListPost(newEvent)
+            if(resp){
+                console.log(resp)
+                const addedEvent = resp.map(event => new Event(event))
+                this.events = this.events.concat(addedEvent)
+                const content = addedEvent[0]
+                this.updateEventList(content,false)
+                if(this.helper.isThisWeek(eventDateAsDate)){
+                    this.updateCalendar(content,false)
+                }
+            }else{
+                console.log(`Failed to add Event Error: ${resp.status}`)
             }
-        }else{
-            console.log(`Failed to add Event Error: ${resp.status}`)
         }
     }
 
-    updateEventModifier(eventSummary){
-        let eventItems =  eventSummary.innerText.split(':')
-        let htmlDate = this.helper.formatDate(eventItems[1])
-        document.getElementById('eventName').value = eventItems[0]
-        document.getElementById('eventDate').value = htmlDate
-    }
-
-    async deleteEventButton(){
-        const eventName = document.getElementById('eventName').value
-        const eventDate = document.getElementById('eventDate').value
+    async delEventButton(eventName,eventDate){
+        console.log(eventName)
+        console.log(eventDate)
         if(eventName === '' || eventDate === ''){return}
         const eventDateAsDate = new Date(eventDate)
         eventDateAsDate.setDate(eventDateAsDate.getDate() + 1)
