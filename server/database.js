d046a4fa0d81371591262876de61a49f291c890d
dannyp99 <dannypires99@gmail.com>
2020-06-10 23:47:49 -0400

    database changed to always run locally

--- server/database.js
+++ server/database.js
@@ -5,9 +5,9 @@ var mysql = require('promise-mysql');
 const db = {};
 //local mysql db connection
 var connection = mysql.createConnection({
-    host     : '34.73.226.81',
-    user     : 'dannypires99',
-    password : 'goodyear',
+    host     : '127.0.0.1',
+    user     : 'pi',
+    password : 'goodyear123!',
     database : 'event_manager',
     supportBigNumbers: true,
     bigNumberStrings: true
